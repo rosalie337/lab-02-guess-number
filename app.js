3fadbe3cb981c72727168a10790d739b9f78b260
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-09-29 20:07:38 -0400

    added app.js and utils.js

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 // import functions and grab DOM elements
+import { resetGame, updateDom } from './utils.js'
+
+const playButton = document.getElementById('play-button');
+const resetButton = document.getElementById('reset-button');
 
 // initialize state
+let attempts = 5;
 
 // set event listeners to update state and DOM
+playButton.addEventListener('click', () => {
+    attempts--;
+    updateDom(attempts);
+});
+
+resetButton.addEventListener('click', () => {
+    resetGame(attempts);
+});
---
+++ utils.js
@@ -0,0 +1,11 @@
+
+const attemptSpan = document.getElementById('attempts-span');
+const resultSpan = document.getElementById('results-span');
+
+export function resetGame(attempts, attemptSpan, results, resultSpan);
+    attempts = 5;
+
+    attemptSpan.textContent = attempts;
+}
+
+export function updateDom(attempts)
